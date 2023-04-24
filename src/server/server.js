014bc890e973486364b20584d2a02f2593c23db9
Pierre <Pierre.Mingels@protonmail.com>
2023-04-24 20:18:50 +0200

    added a build folder for deployment and fixed my server file

--- src/server/server.js
+++ src/server/server.js
@@ -62,12 +62,17 @@ app.post('/login', (req, res) => {
 
 const PORT = process.env.PORT || 3001;
 
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
+app.use(express.static(path.join(__dirname, '../build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
+  
 
   
   
