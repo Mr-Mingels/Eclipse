dc4768d9a03d92ce938e09360c434a480be3357a
Pierre <Pierre.Mingels@protonmail.com>
2023-04-24 19:17:28 +0200

    added route handler

--- src/server/server.js
+++ src/server/server.js
@@ -64,3 +64,11 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
   });
+
+
+  app.get('/', (req, res) => {
+    res.send('Hello, world!');
+  });
+  
+
+
