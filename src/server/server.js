e7d06ef9eb1f652da892027af874949a82e65d14
Pierre <Pierre.Mingels@protonmail.com>
2023-04-24 20:03:30 +0200

    updated server code again

--- src/server/server.js
+++ src/server/server.js
@@ -67,12 +67,13 @@ app.listen(PORT, () => {
   });
 
   if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
+    app.use(express.static(path.join(__dirname, 'client', 'build')));
   
     app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+        res.sendFile(path.join(__dirname, 'public', 'index.html'));
     });
-  }
+}
+
   
   
 
