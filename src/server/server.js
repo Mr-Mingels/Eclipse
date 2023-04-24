0804a49e72166d6ee9d9e61d0e7cded377ad6904
Pierre <Pierre.Mingels@protonmail.com>
2023-04-24 20:08:35 +0200

    updated server code

--- src/server/server.js
+++ src/server/server.js
@@ -62,17 +62,12 @@ app.post('/login', (req, res) => {
 
 const PORT = process.env.PORT || 3001;
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
-  });
+});
 
-  if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, 'client', 'build')));
-  
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, 'public', 'index.html'));
-    });
-}
 
   
   
