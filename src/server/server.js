47dad9b54d745d5144a9881c3e754e1d80b08938
Pierre <Pierre.Mingels@protonmail.com>
2023-04-24 20:24:00 +0200

    updated server file

--- src/server/server.js
+++ src/server/server.js
@@ -63,10 +63,10 @@ app.post('/login', (req, res) => {
 const PORT = process.env.PORT || 3001;
 
 
-app.use(express.static(path.join(__dirname, '../build')));
+app.use(express.static(path.join(__dirname, '../../build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../../build', 'index.html'));
 });
 
 app.listen(PORT, () => {
